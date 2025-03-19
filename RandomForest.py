09dc1ca323ec74463e7b78aa18a625a805de8939
LiZeNi <2623546261@qq.com>
2025-03-19 22:38:03 +0800

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -1,37 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,RandomizedSearchCV
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 from math import log
 import operator
 
-# 加载 Iris 数据集
+
+
+
+# Iris 数据引入
 iris = load_iris()
 X, y = iris.data, iris.target
+print("feature_name{}".format(iris.feature_names))
 
 # 划分数据集
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+print("数据的示例:{}".format(X_train[0]))
+print("label的示例:{}".format(y_train[0]))
 
 #定义决策树
-def CreatTree(dataset, labels, featlabels):
+def CreatTree(dataset, labels, featlabels,bestfeatlabels,first_call=False):#数据（特征值和最后一列标签），特征名称，记录特征树各节点
+    #将特征数据与标签数据拼接
+    if first_call==True:#第一次输入数据
+        labels=labels.reshape(-1,1)
+        dataset=np.concatenate((dataset,labels))
+    else:
+        pass
+    #定义两种结束方式
     classlist = [example[-1] for example in dataset]
     if classlist.count(classlist[0]) == len(classlist):  # 所有类别相同
-        return classlist[0]
-    if len(dataset[0]) == 1:  # 到叶子了
+        return classlist[0]#返回预测值
+    if len(dataset[0]) == 1: # 到叶子了
         return majorityCnt(classlist)
     
     bestfeat = BestFeature(dataset)  # 返回的是特征索引
-    bestfeatlabel = labels[bestfeat]  # 获取对应的特征名称
-    featlabels.append(bestfeatlabel)
+    bestfeatlabel = featlabels[bestfeat]  # 获取对应的特征名称
+    bestfeatlabels.append(bestfeatlabel)
     myTree = {bestfeatlabel: {}}  # 使用特征名称作为键
-    sublabels = labels[:]
+    sublabels = featlabels[:]
     np.delete(sublabels,bestfeat)  # 删除已使用的特征
     featvalues = [example[bestfeat] for example in dataset]
     uniquevalues = set(featvalues)  
     for value in uniquevalues:
         myTree[bestfeatlabel][value] = CreatTree(
-            splitdataset(dataset, bestfeat, value), sublabels, featlabels)
+            splitdataset(dataset, bestfeat, value),labels,featlabels=sublabels, bestfeatlabels=bestfeatlabels, first_call=False)
     
     return myTree
 
@@ -100,8 +113,9 @@ class DecisionTree:
         self.tree = self.BuildTree(X, y)
 
     def BuildTree(self, X, y):
-        featlabels = list(iris.feature_names)  # 使用特征名称列表
-        return CreatTree(X, y, featlabels)
+        bestfeatlabels=[]
+        labels = list(iris.feature_names)  # 获得特征名称列表
+        return CreatTree(X, y,labels, bestfeatlabels)
 
 
 
@@ -109,22 +123,11 @@ class DecisionTree:
         return [self.Predict(inputs) for inputs in X]
     
     def Predict(self, inputs): 
-        tree = self.tree
+        tree = self.tree#  从根节点开始
         while isinstance(tree, dict):
-            feature = int(next(iter(tree)))  # 获取特征名称
-            if feature>3:
-                feature=3
-            if feature==0:
-                name='sepal length (cm)'
-            elif feature ==1:
-                name='sepal width (cm)'
-            elif feature==2:
-                name= 'petal length (cm)'
-            else :
-                name='petal width (cm)'
-
-            value = inputs[iris.feature_names.index(name)]  # 根据特征名称获取输入值
-            if value in tree[feature]:
+            feature = next(iter(tree)) # 获取特征名称
+            value = inputs[iris.feature_names.index(feature)]  # 根据特征名称获取输入值
+            if value in tree[feature]:#根据特征值进入对应子树进行分类
                 tree = tree[feature][value]
             else:
                 return None
@@ -133,16 +136,16 @@ class DecisionTree:
    
 # 定义随机森林
 class RandomForest:
-    def __init__(self, num_trees=10, max_depth=None):
+    def __init__(self, num_trees, max_depth=None):
         self.num_trees = num_trees
         self.max_depth = max_depth
         self.trees = []
         self.feature_importances_ = None
-        self.feature_name_to_index = {name: idx for idx, name in enumerate(iris.feature_names)}  # 特征名称到索引的映射
+        self.feature_name_to_index = {name:idx for idx, name in enumerate(iris.feature_names)}  # 特征名称到索引的映射
 
     def fit(self, X, y):
         self.feature_importances_ = np.zeros(X.shape[1])  # 根据特征数量初始化特征重要性数组
-        for _ in range(self.num_trees):
+        for _ in range(self.num_trees):#构建森林内的树
             sample_indices = np.random.choice(X.shape[0], size=X.shape[0], replace=True)
             X_sample, y_sample = X[sample_indices], y[sample_indices]
             tree = DecisionTree(max_depth=self.max_depth)
@@ -158,26 +161,26 @@ class RandomForest:
 
     def update_feature_importances(self, tree, importance=1.0):
         if isinstance(tree, dict):
-            feature_index = int(next(iter(tree)))  # 获取特征名称
-            if feature_index>3:
-                feature_index=3
+            feature_name = next(iter(tree)) # 获取特征名称
+            feature_index = self.feature_name_to_index[feature_name]#获取特征索引
             self.feature_importances_[feature_index] += importance
-            for subtree in tree.values():
-                self.update_feature_importances(subtree, importance=importance / len(tree))
+            for value_tree in tree.values():#取出当前特征不同取值对应的不同子树
+                for subtree in value_tree.values():#递归遍历所有节点
+                 self.update_feature_importances(subtree, importance=importance / len(tree))
 
     def predict(self, X):
-        predictions = [tree.predict(X) for tree in self.trees]
+        predictions = [tree.predict(X) for tree in self.trees]# 获得预测值
         predictions = np.array(predictions).T  # 每一列代表一个样本
-
         final_predictions = []
+        #取被所有树预测次数最多的为森林的输出
         for pred in predictions:
             # 去除 None 值
             pred = pred[pred != None]
-            # 如果预测结果为空，使用默认值填充（例如，使用最常见的类别）
+            # 如果预测结果为空，使用0填充
             if len(pred) == 0:
                 final_predictions.append(0)  
                 continue
-            # 如果预测结果是非整数类型（如字符串），将其映射为整数
+            # 如果预测结果不是整数，将其映射为整数
             if not np.issubdtype(pred.dtype, np.integer):
                 unique_labels, pred = np.unique(pred, return_inverse=True)
             counts = np.bincount(pred)
@@ -186,21 +189,48 @@ class RandomForest:
             if not np.issubdtype(pred.dtype, np.integer):
                 most_common_label = unique_labels[most_common_label]
 
+            #将森林的总预测值输出
             final_predictions.append(most_common_label)
 
         return np.array(final_predictions)
 
-# 开始训练
-train = RandomForest(num_trees=10, max_depth=3)
-train.fit(X_train, y_train)
+#找到最佳参数
+max_accuracy=0
+best_num_trees=0
+num_trees_dist=np.arange(5,200,5)
+#多轮寻找减小随机采样的影响
+for i in range(5):
+    for num_trees in num_trees_dist:
+        train=RandomForest(num_trees,max_depth=None)
+        train.fit(X_train,y_train)
 
-# 开始检验
-y_pred = train.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy:.2f}')
+
+        # 开始检验
+        y_pred = train.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        if accuracy>max_accuracy:
+            max_accuracy=accuracy
+            best_num_trees=num_trees
+    print("best_num_trees:{}".format(best_num_trees))
+print("best_num_trees:{}".format(best_num_trees))
+
+
+
+#训练最佳模型
+train=RandomForest(best_num_trees,max_depth=None)
+train.fit(X_train,y_train)
+
+#检验
+y_pred=train.predict(X_test)
+accuracy=accuracy_score(y_test,y_pred)
+print(f"accuracy:{accuracy:.2f}")
+
+
 
 # 特征重要性评估
 feature_importances = train.feature_importances_
+sum_importance=np.sum(feature_importances)
+feature_importances/=sum_importance
 print("Feature Importances:", feature_importances)
 
 # 特征重要性可视化
@@ -209,7 +239,6 @@ plt.bar(iris.feature_names, feature_importances, color='skyblue')
 plt.xlabel('Features')
 plt.ylabel('Importance')
 plt.title('Feature Importances')
-plt.xticks(rotation=45)
 plt.show()
 
 # 验证集预测结果可视化
